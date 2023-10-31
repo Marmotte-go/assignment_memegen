5777894fbf2cd2a11fb0f66cb19caa611b1fd8b7
Yuanke <yuankemiao.dev@gmail.com>
2023-10-31 12:47:59 +0200

    main function is done, also added download click

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,78 @@
-import './App.scss';
+import "./App.scss";
+import { useEffect, useState } from "react";
+import Modal from "./components/Modal";
 
 function App() {
+  const [memes, setMemes] = useState([]);
+  const [displayCount, setDisplayCount] = useState(10);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentMeme, setCurrentMeme] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.memegen.link/templates")
+      .then((data) => data.json())
+      .then((res) => {
+        setMemes(res);
+      });
+  }, []);
+
+  const loadMore = () => {
+    setDisplayCount((prev) => prev + 10);
+  };
+
+  const handleOpenModal = (id, name, blank, lines) => {
+    setIsModalOpen(true);
+    setCurrentMeme({ id, name, blank, lines });
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  console.log(currentMeme);
+
   return (
     <div className="App">
-    
+      <header>
+        <h1>Meme Generator</h1>
+        <p>Choose a template you want</p>
+      </header>
+
+      {isModalOpen && (
+          <Modal
+            id={currentMeme.id}
+            name={currentMeme.name}
+            blank={currentMeme.blank}
+            lines={currentMeme.lines}
+            handleCloseModal={handleCloseModal}
+          />
+        )}
+
+      <section className="templates">
+        {memes?.slice(0, displayCount).map((meme) => (
+          <div key={meme.id} className="template">
+            <img src={meme.blank} alt={meme.name} loading="lazy" />
+            <p>{meme.name}</p>
+            <button
+              className="btn-gen"
+              onClick={() =>
+                handleOpenModal(meme.id, meme.name, meme.blank, meme.lines)
+              }
+            >
+              Generate
+            </button>
+          </div>
+        ))}
+      </section>
+
+      <button className="btn-load" onClick={loadMore}>
+        Load more ...
+      </button>
+
+      <footer>
+        <div className="divider">M.Meme</div>
+        <p>Have fun!</p>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,85 @@
+import "./Modal.scss";
+import { useState } from "react";
+
+const Modal = ({ id, name, blank, lines, handleCloseModal }) => {
+  const arrayLines = Array.from(Array(lines).keys());
+
+  const [text, setText] = useState({}); //this will be an object with the index of the input as key and the value of the input as value
+  const [newImg, setNewImg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false); //this will be used to show a loading spinner while the image is being generated
+
+  const handleChange = (e) => {
+    const currentInput = e.target.attributes.getNamedItem("data-index")?.value; //it is the index of the input
+    if (currentInput) {
+      setText((prev) => ({ ...prev, [currentInput]: e.target.value }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    let memeImg = `https://api.memegen.link/images/${id}/`;
+    const layers = Object.values(text);
+
+    layers.forEach((layer, i) => {
+      if (i === layers.length - 1) {
+        memeImg += encodeURIComponent(`${layer}.png`);
+        return;
+      }
+      memeImg += encodeURIComponent(`${layer}/`);
+    });
+
+    setNewImg(memeImg);
+  };
+
+  const handleDownload = () => {
+    //create blob to solve the cross origin download problem
+    fetch(newImg || blank)
+      .then((res) => res.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = `${name}`;
+        a.click();
+      });
+  }
+
+  return (
+    <div className="modal-container">
+      <div className="modal-content">
+        <form onSubmit={handleSubmit}>
+          {arrayLines.map((line, i) => (
+            <input
+              key={line}
+              data-index={i}
+              type="text"
+              placeholder="Add your text on meme"
+              onChange={handleChange}
+            />
+          ))}
+          {isLoading && <div className="loading">Generating ...</div>}
+          <div className="img-container" onClick={handleDownload}>
+            <img
+              src={newImg || blank}
+              alt={name}
+              className="memeImg"
+              onLoad={() => setIsLoading(false)}
+            />
+            <span className="tooltip">Click to download</span>
+          </div>
+
+          <div className="btn-group">
+            <button type="submit">Generate</button>
+            <button className="close-btn" onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
